85cc64407b61a101ad9f392476cf21a084b60fdc
Mark Steve Samson <hello@marksteve.com>
2016-12-26 10:37:22 +0800

    Use js directly for paper

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
+var paper = window.paper
+
+paper.setup(document.getElementById('photo'))
+
+var tool = new paper.Tool()
 var path
 
-function onMouseDown (event) {
-  path = new Path()
+tool.onMouseDown = function (event) {
+  path = new paper.Path()
   path.strokeColor = 'black'
   path.strokeWidth = 3
 }
 
-function onMouseDrag (event) {
+tool.onMouseDrag = function (event) {
   if (path.segments.length > 1) {
     path.lastSegment.point.x = event.point.x
     path.lastSegment.point.y = event.point.y
@@ -15,12 +20,13 @@ function onMouseDrag (event) {
   }
 }
 
-function onMouseUp (event) {
+tool.onMouseUp = function (event) {
   if (path.segments.length < 2) {
-		return
-	}
-  var arrow = new Path.RegularPolygon(event.point, 3, 9)
+    return
+  }
+  var arrow = new paper.Path.RegularPolygon(event.point, 3, 9)
   arrow.fillColor = 'black'
-  var vector = path.lastSegment.point - path.firstSegment.point
+  var vector = path.lastSegment.point.subtract(path.firstSegment.point)
   arrow.rotate(vector.angle + 90)
 }
+
