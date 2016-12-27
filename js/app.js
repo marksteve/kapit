393fdf395224fc28aa1de2223bd6e7863546551c
Mark Steve Samson <hello@marksteve.com>
2016-12-27 23:40:01 +0800

    Add upload progress

--- js/app.js
+++ js/app.js
@@ -14,6 +14,40 @@ firebase.initializeApp(firebaseConfig)
 var database = firebase.database()
 var storage = firebase.storage()
 
+var gyms = [
+  { id: 'ts', name: 'Power Up TS' },
+  { id: 'centro', name: 'Power Up Centro' },
+  { id: 'rox', name: 'Power Up ROX' },
+  { id: 'ccm', name: 'Climb Central Manila' }
+]
+
+var gradings = {
+  freeClimb: [
+    {
+      id: 'yds',
+      name: 'YDS',
+      grades: [ '3-4', '5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', '5.10a', '5.10b', '5.10c', '5.10d', '5.11a', '5.11b', '5.11c', '5.11d', '5.12a', '5.12b', '5.12c', '5.12d', '5.13a', '5.13b', '5.13c', '5.13d', '5.14a', '5.14b', '5.14c', '5.14d', '5.15a', '5.15b', '5.15c' ]
+    },
+    {
+      id: 'french',
+      name: 'French',
+      grades: [ '1', '2', '3', '4a', '4b', '4c', '5a', '5b', '5c', '6a', '6a+', '6b', '6b+', '6c', '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b', '8b+', '8c', '8c+', '9a', '9a+', '9b', '9b+' ]
+    }
+  ],
+  bouldering: [
+    {
+      id: 'hueco',
+      name: 'Hueco',
+      grades: [ 'VB', 'V0-', 'V0', 'V0+', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17' ]
+    },
+    {
+      id: 'font',
+      name: 'Font',
+      grades: [ '3', '4-', '4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C', '7C+', '8A', '8A+', '8B', '8B+', '8C', '8C+', '9A' ]
+    }
+  ]
+}
+
 var Index = {
   template: '#index',
   props: ['user'],
@@ -76,40 +110,6 @@ Vue.component('paper', {
   }
 })
 
-var gyms = [
-  { id: 'ts', name: 'Power Up TS' },
-  { id: 'centro', name: 'Power Up Centro' },
-  { id: 'rox', name: 'Power Up ROX' },
-  { id: 'ccm', name: 'Climb Central Manila' }
-]
-
-var gradings = {
-  freeClimb: [
-    {
-      id: 'yds',
-      name: 'YDS',
-      grades: [ '3-4', '5.0', '5.1', '5.2', '5.3', '5.4', '5.5', '5.6', '5.7', '5.8', '5.9', '5.10a', '5.10b', '5.10c', '5.10d', '5.11a', '5.11b', '5.11c', '5.11d', '5.12a', '5.12b', '5.12c', '5.12d', '5.13a', '5.13b', '5.13c', '5.13d', '5.14a', '5.14b', '5.14c', '5.14d', '5.15a', '5.15b', '5.15c' ]
-    },
-    {
-      id: 'french',
-      name: 'French',
-      grades: [ '1', '2', '3', '4a', '4b', '4c', '5a', '5b', '5c', '6a', '6a+', '6b', '6b+', '6c', '6c+', '7a', '7a+', '7b', '7b+', '7c', '7c+', '8a', '8a+', '8b', '8b+', '8c', '8c+', '9a', '9a+', '9b', '9b+' ]
-    }
-  ],
-  bouldering: [
-    {
-      id: 'hueco',
-      name: 'Hueco',
-      grades: [ 'VB', 'V0-', 'V0', 'V0+', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17' ]
-    },
-    {
-      id: 'font',
-      name: 'Font',
-      grades: [ '3', '4-', '4', '4+', '5', '5+', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C', '7C+', '8A', '8A+', '8B', '8B+', '8C', '8C+', '9A' ]
-    }
-  ]
-}
-
 var Submit = {
   template: '#submit',
   props: ['user'],
@@ -122,6 +122,8 @@ var Submit = {
       routeGrading: null,
       routeGrade: null,
       routePhoto: null,
+      routePhotoUploading: false,
+      routePhotoProgress: 0,
       routeSetter: null,
       routeName: null,
       routeDescription: null
@@ -134,7 +136,8 @@ var Submit = {
           return this.routeGym &&
             this.routeType &&
             this.routeGrading &&
-            this.routeGrade
+            this.routeGrade &&
+            !this.routePhotoUploading
         case 'editPhoto':
           return this.routePhoto
         case 'submitRoute':
@@ -182,15 +185,19 @@ var Submit = {
       var uploadTask = routeImages.put(file)
       uploadTask.on(
         firebase.storage.TaskEvent.STATE_CHANGED,
-        function (snapshot) {
-          console.log('progress', snapshot.bytesTransferred / snapshot.totalBytes * 100)
-        },
+        this.setPhotoProgress,
         function (error) {
           console.error(error)
         },
         this.setPhoto.bind(this, 'submitRoute', uploadTask))
+      this.routePhotoUploading = true
+    },
+    setPhotoProgress: function (snapshot) {
+      this.routePhotoProgress = Math.ceil(snapshot.bytesTransferred / snapshot.totalBytes * 100)
     },
     setPhoto: function (nextStep, uploadTask) {
+      this.routePhotoProgress = 100
+      this.routePhotoUploading = false
       this.routePhoto = uploadTask.snapshot.downloadURL
       this.step = nextStep
     },
